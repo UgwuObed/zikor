19147304e6cef13bbd2b50bd4d1ba577b4272509
UgwuObed <vjazzyvictor@gmail.com>
2024-01-23 18:50:10 +0100

    added admin to zikor backend

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Admin Dashboard
+        </h2>
+    </x-slot>
+
+    <!-- Add your dashboard content here -->
+</x-app-layout>
